376e0c284afc6d110b0378a8f610992b09979fb2
PelicanDistress <carteraxwebsolution@gmail.com>
2021-11-17 22:34:06 +0100

    📦 NEW: development branch

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,9 +1,11 @@
 export enum SupportedChainId {
   BSC = 56,
   RINKEBY = 4,
+  LOCALHOST = 1337,
 }
 
 export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = [
   SupportedChainId.BSC,
   SupportedChainId.RINKEBY,
+  SupportedChainId.LOCALHOST,
 ];
